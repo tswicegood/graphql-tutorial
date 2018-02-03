a5e6207a0fb7ab8d7228d655285607e52108a386
Travis Swicegood <travis@domain51.com>
2018-02-03 14:04:57 -0600

    Add signin

---
+++ test/graphql/resolvers/sign_in_user_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class Resolvers::SignInUserTest < ActiveSupport::TestCase
+  def perform(args = {})
+    Resolvers::SignInUser.new.call(nil, args, { cookies: {}})
+  end
+
+  setup do
+    @user = User.create!(
+      name: "test",
+      email: "test@example.com",
+      password: "test"
+    )
+  end
+
+  test 'creates a token' do
+    result = perform(
+      email: {
+        email: @user.email,
+        password: @user.password
+      }
+    )
+
+    assert result.present?
+    assert result.token.present?
+    assert_equal result.user, @user
+  end
+
+  test 'handles no credentials' do
+    assert_nil perform
+  end
+
+  test 'handles wrong email' do
+    assert_nil perform({email: {email: 'wrong'}})
+  end
+
+  test 'handles wrong password' do
+    assert_nil perform({email: {email: @user.email, password: 'wrong'}})
+  end
+end
