db2de68f825f6a5158943315c9265597ae43a94d
Travis Swicegood <travis@domain51.com>
2018-02-03 09:07:04 -0600

    Initial code through mutations

---
+++ app/graphql/types/link_type.rb
@@ -0,0 +1,8 @@
+Types::LinkType = GraphQL::ObjectType.define do
+  name 'Link'
+  description 'Links represent the core of this application'
+
+  field :id, !types.ID, description: 'Auto generated'
+  field :url, !types.String, description: 'URL, yo'
+  field :description, !types.String, description: 'Pithy and 100% description'
+end
